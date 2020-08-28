8dc8479f8b2222dec431e1eede43a0e58afe794a
kylichist <pimenovpetya@gmail.com>
2020-08-28 17:10:41 +0300

    code cleanup

--- src/main/kotlin/com/y9san9/kds/utils/FileUtils.kt
+++ src/main/kotlin/com/y9san9/kds/utils/FileUtils.kt
@@ -2,12 +2,9 @@ package com.y9san9.kds.utils
 
 import java.io.File
 
-
 internal fun File.refresh(directory: Boolean = false, defaultFileText: String = "") = when {
     exists() -> Unit
-    directory -> {
-        mkdirs().let { }
-    }
+    directory -> mkdirs().let { }
     else -> {
         parentFile.mkdirs()
         createNewFile()
